adae66bf4638b70d8f8da4baa97b2ec06ac541d1
dabao1955 <195328750@qq.com>
2023-04-12 14:10:51 +0800

    update

--- image/jpg/rename.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-i=1
-for f in *.jpg; do
-  mv -- "$f" "$i.jpg"
-  echo "Renamed $f to $i.jpg"
-  i=$((i+1))
-done
-
--- image/png/rename.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-i=1
-for f in *.png; do
-  mv -- "$f" "$i.png"
-  echo "Renamed $f to $i.jpg"
-  i=$((i+1))
-done
-
